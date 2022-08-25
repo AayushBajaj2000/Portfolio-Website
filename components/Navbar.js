8411e03e596d124113fe14c153dcc62a928c9ff8
AayushBajaj2000 <aayubajaj2000@gmail.com>
2022-08-25 00:28:14 -0400

    svg animation and hero text

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import { Button } from "react-bootstrap";
 
 const draw = {
   hidden: { pathLength: 0, opacity: 0 },
@@ -19,8 +20,8 @@ const draw = {
 
 const Navbar = () => {
   return (
-    <div className="h-[70px] w-full justify-around flex fixed p-3 items-center">
-      <div className="flex h-full items-center">
+    <div className="h-[90px] w-full justify-around flex fixed p-3 items-center z-10">
+      <div className="flex items-center h-full">
         <motion.div
           whileHover={{
             x: 0,
@@ -33,7 +34,7 @@ const Navbar = () => {
               type: "spring",
             },
           }}
-          className="flex items-center w-8 h-full mb-2 cursor-pointer"
+          className="flex items-center w-12 h-full mb-2 cursor-pointer"
         >
           <motion.svg
             width="60"
@@ -83,7 +84,7 @@ const Navbar = () => {
               stroke="white"
               stroke-width="18"
               variants={draw}
-              custom={3}
+              custom={2.5}
             />
             <motion.line
               x1="157"
@@ -94,7 +95,7 @@ const Navbar = () => {
               stroke-width="18"
               stroke-linecap="round"
               variants={draw}
-              custom={4}
+              custom={3}
             />
             <motion.line
               x1="156"
@@ -104,16 +105,16 @@ const Navbar = () => {
               stroke="white"
               stroke-width="18"
               variants={draw}
-              custom={5}
+              custom={4}
             />
           </motion.svg>
         </motion.div>
-        <p className="ml-10 font-heading font-bold text-xl hover:cursor-pointer">
+        <p className="ml-10 text-3xl font-semibold font-heading hover:cursor-pointer">
           Aayush Bajaj
         </p>
       </div>
-      <div className="flex justify-around w-[300px]">
-        <p className="hover:underline hover:cursor-pointer">Projects</p>
+      <div className="flex justify-around w-[350px] items-center">
+        <p className="text-xl hover:underline hover:cursor-pointer">Projects</p>
         <div className="flex items-center">
           <svg
             className="mr-2 "
@@ -121,14 +122,18 @@ const Navbar = () => {
             fill="currentColor"
             strokeWidth="0"
             viewBox="0 0 512 512"
-            height="1em"
-            width="1em"
+            height="1.4em"
+            width="1.4em"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z"></path>
           </svg>
-          <p className="hover:underline hover:cursor-pointer">Source</p>
+
+          <p className="text-xl hover:underline hover:cursor-pointer">Source</p>
         </div>
+        <Button className="h-full p-2 text-xl rounded-md outline-2 hover:bg-slate-300 hover:bg-opacity-30 outline-white outline">
+          Resume
+        </Button>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,105 @@
+import { motion } from "framer-motion";
+import { useEffect } from "react";
+
 const Home = () => {
   return (
-    <section className="min-h-screen flex items-center justify-center text-white">
-      <p className="font-heading font-bold text-4xl">Hello Next.js</p>
-    </section>
+    <>
+      <section className="relative flex items-center justify-center min-h-screen text-white">
+        <div className="flex flex-col items-start justify-center max-w-[1000px]">
+          <div className="backdrop-blur-md bg-[rgba(255,255,255,0.08)] p-2 rounded-md">
+            <h1 className="text-xl">Hi, my name is</h1>
+          </div>
+          <div className="my-3">
+            <h2 className="text-6xl font-semibold font-heading">
+              Aayush Bajaj.
+            </h2>
+          </div>
+          <div className="my-3">
+            <h3 className="font-bold text-7xl opacity-70">
+              I am a full stack developer.
+            </h3>
+          </div>
+          <div className="max-w-xl my-3 text-justify">
+            <p>
+              I’m a software engineer specializing in building (and occasionally
+              designing) exceptional digital experiences. Currently, I’m focused
+              on building accessible, human-centered products at Upstatement.
+            </p>
+          </div>
+        </div>
+        <motion.div
+          className="absolute bottom-0"
+          initial={{
+            y: 0,
+            opacity: 1,
+          }}
+          animate={{
+            translateY: -10,
+            transition: {
+              delay: 0.5,
+              duration: 0.5,
+              type: "spring",
+              yoyo: Infinity,
+            },
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-12 h-12"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </motion.div>
+      </section>
+      <section className="relative flex items-center justify-center min-h-screen text-white">
+        <div className="flex flex-col items-start justify-center max-w-[1000px]">
+          <div className="backdrop-blur-md bg-[rgba(255,255,255,0.08)] p-2 rounded-md">
+            <h1 className="text-xl">Hi, my name is</h1>
+          </div>
+          <div className="my-3">
+            <h2 className="text-6xl font-semibold font-heading">
+              Aayush Bajaj.
+            </h2>
+          </div>
+          <div className="my-3">
+            <h3 className="font-bold text-7xl opacity-70">
+              I am a full stack developer.
+            </h3>
+          </div>
+          <div className="max-w-xl my-3 text-justify">
+            <p>
+              I’m a software engineer specializing in building (and occasionally
+              designing) exceptional digital experiences. Currently, I’m focused
+              on building accessible, human-centered products at Upstatement.
+            </p>
+          </div>
+        </div>
+        <motion.div className="absolute bottom-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 50 50"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-12 h-12"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </motion.div>
+      </section>
+    </>
   );
 };
 
