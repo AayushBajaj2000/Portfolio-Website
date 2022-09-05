f84e2611cfdb9ad4ea0dbf26ca090be69aa23656
AayushBajaj2000 <aayubajaj2000@gmail.com>
2022-09-05 04:30:35 -0400

    fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "../styles/globals.css";
 import Layout from "@components/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Aayush Bajaj</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,25 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       </Head>
       <body>
         <Main />
