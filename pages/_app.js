60ad628c6dba1d6278900f1b48201c2515a9902c
AayushBajaj2000 <aayubajaj2000@gmail.com>
2022-09-05 23:23:53 -0400

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import Layout from "@components/Layout";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,17 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Script
+        strategy="lazyOnload"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-0SMVZR7HZT"}
+      ></Script>
+      <Script strategy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-0SMVZR7HZT');`}
+      </Script>
     </>
   );
 }
