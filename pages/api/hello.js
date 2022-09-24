84fd3e34ef91ac7d11dbecccd95c1f8d03758c3f
AayushBajaj2000 <aayubajaj2000@gmail.com>
2022-09-24 01:15:10 -0400

    added req headers route just for checking deployment

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import requestIp from "request-ip";
 
 export default function handler(req, res) {
-  res.status(200).json(req.headers);
+  const clientIp = requestIp.getClientIp(req);
+  res.status(200).json({
+    ip: clientIp,
+  });
 }
