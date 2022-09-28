30b3a37ec2d072b9db094cf17cd8c9769f9ae1f7
AayushBajaj2000 <aayubajaj2000@gmail.com>
2022-09-28 15:54:39 -0400

    fixed env

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
-      <Script strategy="lazyOnload">
+      <Script id="ga" strategy="lazyOnload">
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
